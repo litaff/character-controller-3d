279ea5761e19a23b3c9391dd5add551b419d0fc1
litaff <a.gazda2k18@gmail.com>
2024-02-04 20:43:34 +0100

    Added assets

---
+++ Character Controller 3D/Assets/Scripts/CharacterController3D/CharacterController3D.cs
@@ -0,0 +1,12 @@
+namespace CharacterController3D
+{
+    using UnityEngine;
+
+    public class CharacterController3D : MonoBehaviour
+    {
+        [SerializeField]
+        private Rigidbody rigidbody;
+        
+        
+    }
+}
