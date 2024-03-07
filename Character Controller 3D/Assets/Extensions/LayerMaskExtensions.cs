ecf45730e4609a80da98955fe0208b80a81a22f1
litaff <a.gazda2k18@gmail.com>
2024-03-08 00:48:27 +0100

    LayerMaskExtension

---
+++ Character Controller 3D/Assets/Extensions/LayerMaskExtensions.cs
@@ -0,0 +1,12 @@
+namespace Extensions
+{
+    using UnityEngine;
+
+    public static class LayerMaskExtensions
+    {
+        public static bool Contains(this LayerMask layerMask, int layer)
+        {
+            return layerMask == (layerMask | (1 << layer));
+        }
+    }
+}
